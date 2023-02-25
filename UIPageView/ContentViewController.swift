403e792a8798a0d770e164c800fa816cd4d5eb32
Harry Vologodskii <49877717+GarikVologodskiy@users.noreply.github.com>
2023-02-25 18:47:43 +0500

    added storyboards

---
+++ UIPageView/ContentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewController.swift
+//  UIPageView
+//
+//  Created by Igor Vologodskiy on 25.02.2023.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    @IBOutlet weak var presentTextLabel: UILabel!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIPageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPageView
+//
+//  Created by Igor Vologodskiy on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
