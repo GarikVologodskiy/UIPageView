1cb270d49adf7dab32d31abd3f85e4fe4207e431
Harry Vologodskii <49877717+GarikVologodskiy@users.noreply.github.com>
2023-02-26 12:43:47 +0500

    added some logic of presentation

--- UIPageView/PageViewController.swift
+++ UIPageView/PageViewController.swift
@@ -13,23 +13,30 @@ class PageViewController: UIPageViewController {
     "The first page of the presentation about app",
     "The second page of he presentation about some feature",
     "The third page of presentation about some interesting",
-    "The fourth page of presentation about on a good jorney!"
+    "The fourth page of presentation about on a good jorney!",
+    ""
     ]
     
-    let emojiArray = ["🤗", "🤠", "😵‍💫", "🫶"]
+    let emojiArray = ["🤗", "🤠", "😵‍💫", "🫶", ""]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        dataSource = self
+        
         if let contentViewController = showViewControllerAtIndex(0) {
             setViewControllers([contentViewController], direction: .forward, animated: true, completion: nil)
-        }        
+        }
     }
     
     func showViewControllerAtIndex (_ index: Int) -> ContentViewController? {
         
         guard index >= 0 else { return nil }
-        guard index < presentScreenContent.count else { return nil }
+        guard index < presentScreenContent.count else {
+            dismiss(animated: true, completion: nil)
+            return nil
+            
+        }
         guard let contentViewController = storyboard?.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else { return nil }
         
         contentViewController.presentText = presentScreenContent[index]
@@ -40,4 +47,25 @@ class PageViewController: UIPageViewController {
         return contentViewController
     }
 
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        var pageNumber = (viewController as! ContentViewController).currentPage
+        pageNumber -= 1
+        
+        return showViewControllerAtIndex(pageNumber)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        var pageNumber = (viewController as! ContentViewController).currentPage
+        pageNumber += 1
+        
+        return showViewControllerAtIndex(pageNumber)
+    }
+    
+    
 }
